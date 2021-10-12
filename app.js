8dde2e2a5675c872b49daee24b3f15bb508104d0
KikiLP07 <65845464+KikiTheCoder@users.noreply.github.com>
2021-10-12 17:38:29 +0200

    Added States

--- app.js
+++ app.js
@@ -7,16 +7,23 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get("/pin/:pin", (req, res) => {
+app.get("/pin/:pin/:state", (req, res) => {
     if (isNaN(req.params.pin)) {
-        res.send("Not a Number").status(400)
+        res.send("Pin Not a Number").status(400)
+        return
+    }
+    if (isNaN(req.params.state)) {
+        res.send("State Not a Number").status(400)
         return
     }
 
     let lednr = parseInt(req.params.pin)
     let led = new Gpio(lednr, "out")
+    let state = parseInt(req.params.state)
+    
+    console.log(`Pin: ${lednr}\t|\tState: ${state}`);
 
-    led.writeSync(led.readSync() ^ 1);
+    led.writeSync(state);
 
     res.send(`Switched ${req.params.pin}`)
 })
